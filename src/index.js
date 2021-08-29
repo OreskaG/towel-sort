cd52c0277240c675dd1d67709aac4a61c0e3687c
OreskaG <Ctepanrfo@gmail.com>
2021-08-30 00:29:41 +0300

    fix: delete 'for'

--- src/index.js
+++ src/index.js
@@ -1,8 +1,8 @@
+
+// You should implement your task here.
+
 module.exports = function towelSort (matrix) {
-    let result = [];
     if ( matrix === undefined ) { return [] };
-    for ( let i = 0 ; i < matrix.length ; i++ ) {
-        if ( i % 2 !== 0) { result = matrix[i].reverse() };
-    };
-    return result.flat();
+    matrix[1] = matrix[1].reverse();
+    return matrix.flat();
 }
