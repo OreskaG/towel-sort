a4762074203dd1e9d8e77bb44d6b007a9832625a
OreskaG <Ctepanrfo@gmail.com>
2021-08-30 00:39:58 +0300

    fix: add additional check

--- src/index.js
+++ src/index.js
@@ -2,7 +2,9 @@
 // You should implement your task here.
 
 module.exports = function towelSort (matrix) {
-    if ( matrix === undefined ) { return [] };
-    matrix[1] = matrix[1].reverse();
+    if ( matrix === undefined || !matrix ) { return [] };
+    for ( let i = 1 ; matrix[i] ; i = i+2 ) {
+        matrix[i] = matrix[i].reverse()
+    };
     return matrix.flat();
 }
